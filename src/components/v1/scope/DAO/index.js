97fb4534a6029b234199a8f8ec1fd19727019fe8
Camilo Acevedo <camilo-mac@Camilos-MacBook-Pro.local>
2021-09-06 12:23:27 -0500

    Implemented DAO function

--- src/components/v1/scope/DAO/index.js
+++ src/components/v1/scope/DAO/index.js
@@ -5,9 +5,12 @@ const findScopeByNameAndModule = ({ name, module }) => scopeModel.findOne({
   module,
 }).lean().exec();
 
+const findScopeById = (id) => scopeModel.findOne({ _id: id });
+
 const newScope = (scope) => scopeModel.create({ ...scope });
 
 export default {
   findScopeByNameAndModule,
+  findScopeById,
   newScope,
 };
