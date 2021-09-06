31a0011b27319953292bc2868d30f9738ef485a0
Camilo Acevedo <camilo-mac@Camilos-MacBook-Pro.local>
2021-09-06 16:09:50 -0500

    Fix linter issues and some code smells

--- src/components/v1/account/dao/index.js
+++ src/components/v1/account/dao/index.js
@@ -1,12 +1,11 @@
-import accountModel from "../../../../models/account";
+import accountModel from '../../../../models/account';
 
-const findAccountByName = ({ name }) =>
-  accountModel
-    .findOne({
-      name,
-    })
-    .lean()
-    .exec();
+const findAccountByName = ({ name }) => accountModel
+  .findOne({
+    name,
+  })
+  .lean()
+  .exec();
 
 const newAccount = (account) => accountModel.create({ ...account });
 
